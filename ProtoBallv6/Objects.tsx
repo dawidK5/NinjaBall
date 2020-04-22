b82ae056414cc21a1e402b187a4ca4b7b285dacd
Oscarbd01 <60885578+Oscarbd01@users.noreply.github.com>
2020-04-22 11:43:58 +0100

    Create Objects.tsx

---
+++ ProtoBallv6/Objects.tsx
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="2020.03.18" name="Objects" tilewidth="404" tileheight="403" tilecount="12" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="100" height="99" source="../final version of assets + references/bumper made by oscar.png"/>
+ </tile>
+ <tile id="1">
+  <image width="223" height="60" source="../final version of assets + references/flipper made by dawid.png"/>
+ </tile>
+ <tile id="2">
+  <image width="128" height="33" source="../final version of assets + references/shelfcrop.png"/>
+ </tile>
+ <tile id="3">
+  <image width="34" height="51" source="../final version of assets + references/spring.png"/>
+ </tile>
+ <tile id="4">
+  <image width="140" height="61" source="../final version of assets + references/spikes need to look for reference.png"/>
+ </tile>
+ <tile id="5">
+  <image width="154" height="40" source="../../../../Downloads/New folder (2)/ProtoBallv4dCopy/button.png"/>
+ </tile>
+ <tile id="6">
+  <image width="401" height="280" source="../../../../Downloads/New folder (2)/ProtoBallv4dCopy/woodenBackgroud.webp"/>
+ </tile>
+ <tile id="7">
+  <image width="404" height="403" source="../../../../Downloads/New folder (2)/ProtoBallv4dCopy/ninja_ball.png"/>
+ </tile>
+ <tile id="8">
+  <image width="90" height="90" source="../../../../Downloads/New folder (2)/ProtoBallv4dCopy/crate.png"/>
+ </tile>
+ <tile id="9">
+  <image width="401" height="280" source="../.github/woodenBackgroud.webp"/>
+ </tile>
+ <tile id="10">
+  <image width="291" height="321" source="../.github/paper background.jpg"/>
+ </tile>
+ <tile id="11">
+  <image width="291" height="321" source="../.github/paper background.jpg"/>
+ </tile>
+</tileset>
